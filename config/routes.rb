2b0115e2aac65465bbf95b467c6a91660c7eb106
raul1984 <raul.rodrigo1984@outlook.com>
2017-03-13 13:23:35 +0100

    4.3. add order controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+
+  get 'static_pages/landing_page'
+
   resources :object_names
   resources :products
   get 'static_pages/contact'
@@ -7,7 +11,7 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
