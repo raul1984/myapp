034353df1cd0923d6fd3aef17bd65dbfe85aa985
raul1984 <raul.rodrigo1984@outlook.com>
2017-03-22 20:23:37 +0100

    5.1.routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
-  root 'static_pages/index'
+  get 'static_pages/index'
 
-  get 'static_pages#landing_page'
+  root 'static_pages#landing_page'
   
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
