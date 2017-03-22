4d333b46bb2fb8a0ad7e48ed3b30202c31784c90
raul1984 <raul.rodrigo1984@outlook.com>
2017-03-22 20:30:49 +0100

    5.1.RUTAS

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   resources :products
   get 'static_pages/contact'
 
-  root 'static_pages/about'
+  get 'static_pages/about'
 
   get 'static_pages/index'
 
-  get 'static_pages#landing_page'
+  root 'static_pages#landing_page'
   
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
