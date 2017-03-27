f9614595e06b16af8239fa389019ec7ebdc9e363
raul1984 <raul.rodrigo1984@outlook.com>
2017-03-27 19:00:01 +0200

    5.3 mails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,4 @@ class StaticPagesController < ApplicationController
    @products = Product.limit(3)
   end
 
-  def thank_you
-    @name = params[:name]
-    @email = params[:mail]
-    @message = params[:message]
-    UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
-end
+
