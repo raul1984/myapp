6ccfbc44d85792098240cf52bb55d0394fd60bdc
raul1984 <raul.rodrigo1984@outlook.com>
2017-03-30 18:03:35 +0200

    5.4.Nueva rama

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-
-  get 'static_pages/landing_page'
-
-  resources :object_names
+    devise_for :users
+  resources :users
   resources :products
-  get 'static_pages/contact'
-
   get 'static_pages/about'
 
+  get 'static_pages/contact'
+
   get 'static_pages/index'
-
+  root 'static_pages#index'
   post 'static_pages/thank_you'
-
-  root 'static_pages#landing_page'
+  post 'static_pages/index'
   
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
