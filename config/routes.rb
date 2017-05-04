81dc9bdf1f756b03646b1651c6227b055ca0bd9f
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-04 13:15:39 +0200

    6.6 Creating Payments

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
 
   resources :orders, only: [:index, :show, :create, :destroy]
+  post 'payments/create'
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
