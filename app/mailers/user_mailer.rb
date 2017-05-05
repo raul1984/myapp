71eb3f7088680213179ed0632f28bb09a4a068bb
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-05 18:15:06 +0200

    Nuevo Mailer Welcome nuevos users

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,10 +8,27 @@ class UserMailer < ApplicationMailer
       :subject => "A new contact form message from #{name}")
   end
 
-    def welcome_email(user)
-    mail(:to => user.email,
-      :subject => "Welcome to Delicias de la Jara!")
+    def welcome(user)
+        @appname = "DElICIAS DE LA JARA"
+      @user =user
+        mail(:to => @user.email,
+                                :subject => "Welcome to #{@appname}!")
+      UserMailer.welcome(@user).deliver.now
   end
+
+    def order_complete(user, product)
+      @user = user
+      @product = product
+      mail(to: @user.email, subject: 'Thank you for your order!')
+  end
+
+    def thank_you
+      @name = params[:name]
+      @email = params[:email]
+      @message = params[:message]
+      UserMailer.contact_form(@email, @name, @message).deliver_now
+    end
+
 end
 
 
