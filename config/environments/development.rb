1c44d8ddcafee7b47806a997e513f17b2d89edab
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-09 19:37:59 +0200

    6.12 REDIS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,6 @@ Rails.application.configure do
 
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
 end
