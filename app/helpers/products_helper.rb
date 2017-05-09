937c3fd11b82d1a62f81138133ed1688118df65a
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-09 20:01:51 +0200

    Problemas caching

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,7 +1,7 @@
 module ProductsHelper
   def cache_key_for_products
-    count          = Product.count
+    count= Product.count
     max_updated_at = Product.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "products/all-#{count}-#{max_updated_at}"
+    "products/#{params[:q] || "all"}-#{count}-#{max_updated_at}#{signed_in? && current_user.admin? ? "-admin" : "normal"}"
   end
 end
