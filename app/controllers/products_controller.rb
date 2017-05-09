d602e50e0fdba2619d700cd18c9f38ce00e68d6e
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-09 19:44:37 +0200

    6.12. Controller REDIS

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-
+    $redis.set("product:#{@product.id}")
     respond_to do |format|
       if @product.save
         format.html { redirect_to @product, notice: 'Product was successfully created.' }
