44fb64dd33c2260a83585052ed4c4d2980008156
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-10 10:37:37 +0200

    6.9.Arreglo JS

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,18 +1,17 @@
-var refreshRating = function () {
-  $(".rating").raty( { path: '/assets', scoreName: 'comment[rating]' });
-  $(".rated").raty({ path: "/assets",
-    readOnly: true,
-    score: function() {
-      return $(this).attr("data-score");
-    }
-  });
-};
+$(document).on('turbolinks:load', function(){
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
 
-$(document).on("turbolinks:load", function () {
-  refreshRating();
-  
-  $(".image-zoom").elevateZoom({zoomWindowPosition: 10});
-  $(".enable-review").click( function() {
-    $(".review-container").show();
+    /* elevate zoom plugin*/
+    $(".img-zoom").elevateZoom({
+      zoomWindowFadeIn: 500,
+      zoomWindowFadeOut: 500,
+      lensFadeIn: 500,
+      lensFadeOut: 500
   });
 });
