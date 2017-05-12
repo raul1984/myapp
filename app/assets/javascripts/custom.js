de98f073964ca8dbe0e1154a0a3332a15905e544
raul1984 <raul.rodrigo1984@outlook.com>
2017-05-12 20:03:19 +0200

    comentarios

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,14 @@
-$(document).on( 'turbolinks:load', function() { 
-  $(".alert").delay(2000).fadeOut(1000);
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
+
+$(document).on('turbolinks:load', function() {
+  refreshRating();
+  $('.img-zoom').elevateZoom({zoomType: "lens", lensShape: "round", lensSize: 300, scrollZoom: true, lensFadeIn: 500, lensFadeOut: 500});
 });
